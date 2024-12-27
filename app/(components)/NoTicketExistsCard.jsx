226c767e6053a9e308b6fa0fc276d75916afed57
Madhav Dhall <generalgen06@gmail.com>
2024-12-27 17:26:01 +0530

    removed unneccesary line and aligned card contents at center

--- app/(components)/NoTicketExistsCard.jsx
+++ app/(components)/NoTicketExistsCard.jsx
@@ -4,13 +4,12 @@ const NoTicketExistsCard = () => {
     return (
         <div className="lg:grid grid-cols-2 xl:grid-cols-4 gap-4">
             <div
-                className="flex flex-col hover:bg-card-hover bg-card rounded-md shadow-lg p-3 m-2"
+                className="flex flex-col justify-center text-center hover:bg-card-hover bg-card rounded-md shadow-lg p-3 m-2"
             >
                 <h4 className="mb-1">No Tickets Found</h4>
                 <hr className="h-px border-0 bg-page mb-2 " />
                 <p className="whitespace-pre-wrap">There are no tickets available at the moment.</p>
 
-                <div className="flex-grow"></div>
                 <Link href={"/TicketPage/new"} className="btn my-2 max-w-xs bg-blue-600 text-slate-50 hover:bg-blue-700 hover:text-slate-100">Create A Ticket</Link>
             </div>
         </div>
