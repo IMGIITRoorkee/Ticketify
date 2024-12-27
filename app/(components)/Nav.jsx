c0adcc3fbf79b2bcbcffc6f6cf1c29ca0f130be5
disvid <dishikavidhani@gmail.com>
2024-12-28 03:37:46 +0530

    added tooltips to icons

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -1,18 +1,33 @@
+"use client"
 import { faHome, faTicket } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
+import React, { useState } from 'react';
 
 const Nav = () => {
+  const [tooltip, setTooltip] = useState(null);
+
+  const showTooltip = (content) => setTooltip(content);
+  const hideTooltip = () => setTooltip(null);
   return (
     <nav className="flex justify-between bg-nav p-4">
       <div className="flex items-center space-x-4">
-        <Link href="/">
+        <Link href="/"
+        onMouseEnter={() => showTooltip('Home')}
+        onMouseLeave={hideTooltip}>
           <FontAwesomeIcon icon={faHome} className="icon" />
         </Link>
-        <Link href="/TicketPage/new">
+        <Link href="/TicketPage/new"
+        onMouseEnter={() => showTooltip('Create a new ticket')}
+        onMouseLeave={hideTooltip}>
           <FontAwesomeIcon icon={faTicket} className="icon" />
         </Link>
       </div>
+      {tooltip && (
+        <div className="absolute bg-black text-white text-xs py-1 px-2 rounded top-12">
+          {tooltip}
+        </div>
+      )}
       <div>
         
         
