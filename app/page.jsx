02e25c79fa7a91b009abd40cd749efabca70aca7
Madhav Dhall <generalgen06@gmail.com>
2024-12-27 13:27:14 +0530

    Changes made as per review

--- app/page.jsx
+++ app/page.jsx
@@ -47,47 +47,49 @@ const Dashboard = () => {
 
   return (
     <div className="p-5">
-      <div className="mb-4">
-        <label htmlFor="view-by" className="mr-2">
-          View By:
-        </label>
-        <select
-          id="view-by"
-          value={viewBy}
-          onChange={handleViewByChange}
-          className="p-2 border rounded"
-        >
-          <option value="category">Category</option>
-          <option value="status">Status</option>
-        </select>
-      </div>
+      {tickets.length === 0 ? <NoTicketExists /> :
+        <>
+          <div className="mb-4">
+            <label htmlFor="view-by" className="mr-2">
+              View By:
+            </label>
+            <select
+              id="view-by"
+              value={viewBy}
+              onChange={handleViewByChange}
+              className="p-2 border rounded"
+            >
+              <option value="category">Category</option>
+              <option value="status">Status</option>
+            </select>
+          </div>
 
-      <div>
-        {tickets.length === 0 ? <NoTicketExists /> :
-          getUniqueGroups().map((group, groupIndex) => (
-            <div key={groupIndex} className="mb-4">
-              <h2>{group}</h2>
-              <div className="lg:grid grid-cols-2 xl:grid-cols-4 gap-4">
-                {tickets
-                  .filter((ticket) =>
-                    viewBy === "category"
-                      ? ticket.category === group
-                      : ticket.status === group
-                  )
-                  .sort((a, b) => b.priority - a.priority)
-                  .map((filteredTicket, ticketIndex) => (
-                    <TicketCard
-                      id={ticketIndex}
-                      key={ticketIndex}
-                      ticket={filteredTicket}
-                    />
-                  ))}
+          <div>
+            {getUniqueGroups().map((group, groupIndex) => (
+              <div key={groupIndex} className="mb-4">
+                <h2>{group}</h2>
+                <div className="lg:grid grid-cols-2 xl:grid-cols-4 gap-4">
+                  {tickets
+                    .filter((ticket) =>
+                      viewBy === "category"
+                        ? ticket.category === group
+                        : ticket.status === group
+                    )
+                    .sort((a, b) => b.priority - a.priority)
+                    .map((filteredTicket, ticketIndex) => (
+                      <TicketCard
+                        id={ticketIndex}
+                        key={ticketIndex}
+                        ticket={filteredTicket}
+                      />
+                    ))}
+                </div>
               </div>
-            </div>
-          ))}
-      </div>
+            ))}
+          </div>
+        </>}
     </div>
-  );
+  )
 };
 
 export default Dashboard;
