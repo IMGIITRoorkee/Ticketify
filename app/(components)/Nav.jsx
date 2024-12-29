cebc9b8a7bff81c99886c1d1281a9a3efb799d7e
Madhav Dhall <generalgen06@gmail.com>
2024-12-29 20:15:25 +0530

    resolved issue#15 : on the navbar, now it is  clear if home is chosen or create ticket button is chosen

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -1,21 +1,27 @@
+"use client"
 import { faHome, faTicket } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Nav = () => {
+  const currentPath = usePathname()
+
+  const isActive = (href) => currentPath === href;
+
   return (
     <nav className="flex justify-between bg-nav p-4">
       <div className="flex items-center space-x-4">
         <Link href="/">
-          <FontAwesomeIcon icon={faHome} className="icon" />
+          <FontAwesomeIcon icon={faHome} className={`icon ${isActive("/") ? "text-white" : "text-gray-500"}`} />
         </Link>
         <Link href="/TicketPage/new">
-          <FontAwesomeIcon icon={faTicket} className="icon" />
+          <FontAwesomeIcon icon={faTicket} className={`icon ${isActive("/TicketPage/new") ? "text-white" : "text-gray-500"}`} />
         </Link>
       </div>
       <div>
-        
-        
+
+
         <p className=" text-default-text">INFORMATION MANAGEMANT GROUP</p>
       </div>
     </nav>
