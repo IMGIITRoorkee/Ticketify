a9e88fbfb3f6d0ecb463fce1b220d6f313e6e075
disvid <dishikavidhani@gmail.com>
2024-12-29 17:17:50 +0530

    added caching

--- app/api/Tickets/route.js
+++ app/api/Tickets/route.js
@@ -1,9 +1,21 @@
 import Ticket from "@/app/models/Ticket";
+import getRedisClient from "@/app/redisClient";
 import { NextResponse } from "next/server";
 
 export async function GET() {
+  const cacheKey = "tickets";
+  const redisClient = await getRedisClient();
+
   try {
+    const cachedData = await redisClient.get(cacheKey);
+    if (cachedData) {
+      console.log("Cache hit for tickets");
+      return NextResponse.json({ tickets: JSON.parse(cachedData) });
+    }
+
+    console.log("Cache miss for tickets");
     const tickets = await Ticket.find();
+    await redisClient.set(cacheKey, JSON.stringify(tickets), { EX: 3600 });
     return NextResponse.json({ tickets }, { status: 200 });
   } catch (err) {
     console.log(err);
