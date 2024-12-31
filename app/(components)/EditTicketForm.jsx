203365f230cb98de90048af3986bbda3aa904706
Xarvis <nsnishant51@gmail.com>
2024-12-31 15:52:35 +0530

    issue_77 fix category spelling for  "Application Development"

--- app/(components)/EditTicketForm.jsx
+++ app/(components)/EditTicketForm.jsx
@@ -71,7 +71,7 @@ const EditTicketForm = ({ ticket }) => {
   const categories = [
     "Hardware Problem",
     "Software Problem",
-    "Application Deveopment",
+    "Application Development",
     "Project",
     "Bug Fix",
     "MVP"
